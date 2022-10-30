5be57aab4e230a3e4e39a866cf5866d76de083d3
Isaac <isaac.novaes@hotmail.com>
2022-10-30 21:05:02 +0100

    Update App component

--- App.tsx
+++ App.tsx
@@ -1,115 +1,108 @@
-import React, { type PropsWithChildren } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'react-native';
+import InitialScreen from './screens/InitialScreen';
 import {
-    SafeAreaView,
-    ScrollView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    useColorScheme,
-    View,
-} from 'react-native';
+    TutorialStepOneScreen,
+    TutorialStepTwoScreen,
+    TutorialStepThreeScreen,
+    TutorialStepFourScreen,
+} from './screens/TutorialScreens';
+import { type StackScreens } from './types/navigation';
+import LogInScreen from './screens/LogInScreen';
+import SingUpScreen from './screens/SingUpScreen';
+import CreateRoomScreen from './screens/CreateRoomScreen';
+import BottomTabsScreenContainer from './screens/BottomTabs/BottomTabsScreenContainer';
+import { colors } from './Constants/colors';
 
-import {
-    Colors,
-    DebugInstructions,
-    Header,
-    LearnMoreLinks,
-    ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const styles = StyleSheet.create({
-    sectionContainer: {
-        marginTop: 32,
-        paddingHorizontal: 24,
-    },
-    sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
-    },
-    sectionDescription: {
-        marginTop: 8,
-        fontSize: 18,
-        fontWeight: '400',
-    },
-    highlight: {
-        fontWeight: '700',
-    },
-});
-
-const Section: React.FC<
-    PropsWithChildren<{
-        title: string;
-    }>
-> = ({ children, title }) => {
-    const isDarkMode = useColorScheme() === 'dark';
-    return (
-        <View style={styles.sectionContainer}>
-            <Text
-                style={[
-                    styles.sectionTitle,
-                    {
-                        color: isDarkMode ? Colors.white : Colors.black,
-                    },
-                ]}
-            >
-                {title}
-            </Text>
-            <Text
-                style={[
-                    styles.sectionDescription,
-                    {
-                        color: isDarkMode ? Colors.light : Colors.dark,
-                    },
-                ]}
-            >
-                {children}
-            </Text>
-        </View>
-    );
-};
+const Stack = createNativeStackNavigator<StackScreens>();
 
 const App = () => {
-    const isDarkMode = useColorScheme() === 'dark';
-
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    };
-
     return (
-        <SafeAreaView style={backgroundStyle}>
-            <StatusBar
-                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-                backgroundColor={backgroundStyle.backgroundColor}
-            />
-            <ScrollView
-                contentInsetAdjustmentBehavior='automatic'
-                style={backgroundStyle}
-            >
-                <Header />
-                <View
-                    style={{
-                        backgroundColor: isDarkMode
-                            ? Colors.black
-                            : Colors.white,
+        <>
+            <StatusBar barStyle={'light-content'} />
+            <NavigationContainer>
+                <Stack.Navigator
+                    screenOptions={{
+                        headerStyle: { backgroundColor: colors.background },
+                        headerTintColor: colors.green,
+                        // headerTitleStyle: {}
+                        contentStyle: {
+                            backgroundColor: colors.background,
+                        },
+                        headerTitleAlign: 'center',
+                        animation: 'fade',
                     }}
                 >
-                    <Section title='Step One'>
-                        Edit <Text style={styles.highlight}>App.tsx</Text> to
-                        change this screen and then come back to see your edits.
-                    </Section>
-                    <Section title='See Your Changes'>
-                        <ReloadInstructions />
-                    </Section>
-                    <Section title='Debug'>
-                        <DebugInstructions />
-                    </Section>
-                    <Section title='Learn More'>
-                        Read the docs to discover what to do next:
-                    </Section>
-                    <LearnMoreLinks />
-                </View>
-            </ScrollView>
-        </SafeAreaView>
+                    <Stack.Screen
+                        name='Initial'
+                        component={InitialScreen}
+                        options={{
+                            title: 'Welcome',
+                            headerTitleAlign: 'center',
+                        }}
+                    />
+                    <Stack.Screen
+                        name='TutorialStepOne'
+                        component={TutorialStepOneScreen}
+                        options={{
+                            title: 'Tutorial Step 1',
+                            presentation: 'modal',
+                            animation: 'slide_from_bottom',
+                        }}
+                    />
+                    <Stack.Screen
+                        name='TutorialStepTwo'
+                        component={TutorialStepTwoScreen}
+                        options={{
+                            title: 'Tutorial Step 2',
+                            presentation: 'modal',
+                            animation: 'slide_from_bottom',
+                        }}
+                    />
+                    <Stack.Screen
+                        name='TutorialStepThree'
+                        component={TutorialStepThreeScreen}
+                        options={{
+                            title: 'Tutorial Step 3',
+                            presentation: 'modal',
+                            animation: 'slide_from_bottom',
+                        }}
+                    />
+                    <Stack.Screen
+                        name='TutorialStepFour'
+                        component={TutorialStepFourScreen}
+                        options={{
+                            title: 'Tutorial Step 4',
+                            presentation: 'modal',
+                            animation: 'slide_from_bottom',
+                        }}
+                    />
+                    <Stack.Screen
+                        name='Login'
+                        component={LogInScreen}
+                        options={{ title: 'Log in' }}
+                    />
+                    <Stack.Screen
+                        name='SignUp'
+                        component={SingUpScreen}
+                        options={{ title: 'Sign up' }}
+                    />
+                    <Stack.Screen
+                        name='CreateRoom'
+                        component={CreateRoomScreen}
+                        options={{
+                            title: 'Create Room',
+                            headerBackVisible: false,
+                        }}
+                    />
+                    <Stack.Screen
+                        name='BottomTabs'
+                        component={BottomTabsScreenContainer}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </>
     );
 };
 
