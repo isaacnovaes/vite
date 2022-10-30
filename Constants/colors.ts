a95b32368171c9f1a178bfd60cb433dbc1f241d5
Isaac <isaac.novaes@hotmail.com>
2022-10-30 21:04:01 +0100

    Add colors and icons

---
+++ Constants/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+    background: '#1B232A',
+    foreground: '#161C22',
+    darkText: '#171D22',
+    gray: '#3E474F',
+    green: '#5ED5A8',
+    red: '#DD4B4B',
+    white: '#F1F4F6',
+};
